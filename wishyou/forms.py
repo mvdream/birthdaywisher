15dd6ff0710b208291d6ee2ab2807bba30ba60d9
BinoyOza <binoyoza.bo@gmail.com>
2019-09-25 18:34:45 +0530

    Connected Models with the Form to add people. And enabled datepicker for the birthdate field.

--- wishyou/forms.py
+++ wishyou/forms.py
@@ -1,11 +1,21 @@
 from django import forms
 
-class PeopleForm(forms.Form):
+from wishyou.models import People
+
+
+class PeopleForm(forms.ModelForm):
     name = forms.CharField(label="Name:")
     email = forms.CharField(label="Email Id:")
-    birthday = forms.DateField(input_formats=['%d/%m/%Y'],
-        widget=forms.DateInput(attrs={
-            'class': 'form-control datetimepicker-input',
-            'data-target': '#datetimepicker1'
-        }))
-    image = forms.ImageField(label="Image:",widget=forms.FileInput(attrs={'multiple': True}))
+    # birthday = forms.DateField(input_formats=['%d/%m/%Y'],
+    #                            widget=forms.DateInput(attrs={
+    #                                'class': 'form-control datetimepicker-input',
+    #                                'data-target': '#datetimepicker1'
+    #                            }))
+    image = forms.ImageField(label="Image:", widget=forms.FileInput(attrs={'multiple': True}))
+
+    class Meta:
+        model = People
+        fields = ['name', 'email', 'birthday']
+        widgets = {
+            'birthday': forms.DateInput(attrs={'id':"datepicker",'required': True, 'readonly':True}),
+        }
--- wishyou/views.py
+++ wishyou/views.py
@@ -1,30 +1,44 @@
-from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import View
-from django.views.generic.edit import FormView
+from django.views.generic.edit import FormView, CreateView
+
+from wishyou.models import People
 from .forms import PeopleForm
 from django.core.mail import send_mail
 from django.conf import settings
+
+
 class Home(View):
-    def get(self,request):
-        data = {'name':'Mayur'}
-        return render(request,'wishyou/index.html',context=data)
-    
-class PeopleView(FormView):
-    template_name = 'wishyou/form.html'
-    form_class = PeopleForm
-    success_url = reverse_lazy("wishyou:home")
-    def form_valid(self, form):
-        return super().form_valid(form)
+    def get(self, request):
+        data = {'name': 'Mayur'}
+        return render(request, 'wishyou/index.html', context=data)
+
+
+class PeopleView(View):
+    # template_name = 'wishyou/form.html'
+    # model = People
+    # form_class = PeopleForm
+    # success_url = reverse_lazy("wishyou:home")
+    def get(self, request):
+        form = PeopleForm
+        return render(request, 'wishyou/form.html', {'form': form})
+
+    def post(self, request, **kwargs):
+        import code; code.interact(local=dict(globals(), **locals()))
+        return HttpResponse("Hi")
 
 class AddPeople(View):
-    def post(self,request):
+    def post(self, request):
         pass
+
+
 class SendMail(View):
-    def get(self,request):
+    def get(self, request):
         subject = 'Thank you for registering to our site'
         message = ' it  means a world to us '
         email_from = settings.EMAIL_HOST_USER
         recipient_list = ['mvdream02@gmail.com']
-        send_mail( subject, message, email_from, recipient_list )
+        send_mail(subject, message, email_from, recipient_list)
         return redirect(reverse_lazy("wishyou:home"))
